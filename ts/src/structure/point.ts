f6062c37710ed9d187ce10962862c9e8cd8ac868
Trym Nilsen <trym.nilsen@gmail.com>
2019-04-29 22:03:48 +0200

    add point structure

---
+++ ts/src/structure/point.ts
@@ -0,0 +1,18 @@
+export interface Point {
+    x: number;
+    y: number;
+}
+
+export function addPoint(p1: Point, p2: Point) {
+    return {
+        x: p1.x + p2.x,
+        y: p1.y + p2.y
+    };
+}
+
+export function subtractPoint(p1: Point, p2: Point) {
+    return {
+        x: p1.x - p2.x,
+        y: p1.y - p2.y
+    };
+}
